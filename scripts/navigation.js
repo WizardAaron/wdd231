ff49600a83fc8a17337576a153e50d843583e934
WizardAaron <ethanlong547@gmail.com>
2025-09-05 16:20:06 -0700

    Updated main and footer for mobile and desktop pages, certificate section incomplete.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,3 +5,15 @@ navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
     navlinks.classList.toggle('show');
 })
+
+// Update footer with current year and last modified date
+const year = document.querySelector("#year");
+const short = document.querySelector("#short");
+
+if (year) {
+    year.textContent = new Date().getFullYear();
+}
+
+if (short) {
+    short.textContent = document.lastModified;
+}
